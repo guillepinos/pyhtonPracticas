5766b580eeea8a230b7b312e281ad7d50ec5ca40
Guille <guille.pinos.ortiz@gmail.com>
2024-10-28 16:47:25 +0100

    Dejar solo la setencia que provoca la exception en el try

--- maxmin.py
+++ maxmin.py
@@ -6,13 +6,14 @@ while True:
         if num == "done":
             break
         num = int(num)
-        if smallest is None or num < smallest:
-            smallest = num
-        if largest is None or num > largest:
-            largest = num
 
     except:
         print("Invalid input")
+
+    if smallest is None or num < smallest:
+        smallest = num
+    if largest is None or num > largest:
+        largest = num
         
 print("Maximum", largest)
 print("Minimum", smallest)
